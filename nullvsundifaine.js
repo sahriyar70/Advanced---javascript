5ca70b6e45f57d3a9f5703462d9398d3d115212c
AL-Sahriyar <alsahriyar229@gmail.com>
2025-02-20 10:05:48 +0600

    null vs undefined

---
+++ nullvsundifaine.js
@@ -0,0 +1,20 @@
+let pokkhi;
+console.log(pokkhi);
+
+function add(num1, num2){
+    console.log(num1+num2);
+        return
+}
+const result = add(4,7)
+console.log(result)
+
+const pokki ={ name: "sahriyar",id: 354, age: 18,}
+
+console.log(pokki.addeass);
+
+
+ let fun = undefined;
+ console.log (fun);
+
+// or null is a null value
+
