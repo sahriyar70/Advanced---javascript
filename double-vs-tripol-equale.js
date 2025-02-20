6b0ed3d5420ac6ec7ac5466e4e19f1dad2fca0e1
AL-Sahriyar <alsahriyar229@gmail.com>
2025-02-20 11:06:26 +0600

    dobule and tripol equal

---
+++ double-vs-tripol-equale.js
@@ -0,0 +1,12 @@
+
+let frist = 4;
+let last = "4";
+if(frist==last){
+    console.log("it is true")
+
+}
+else{
+    console.log("it is false");
+
+}
+// dobule equal (==) sudhu value chake kore er tripol equal (===) valu and data type dutoi chake koree
