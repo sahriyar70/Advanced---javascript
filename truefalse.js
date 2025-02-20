59bf1ba1209da4f8d97282e74ef8ae40ffcc3f90
AL-Sahriyar <alsahriyar229@gmail.com>
2025-02-20 09:44:10 +0600

    true vs false

---
+++ truefalse.js
@@ -0,0 +1,21 @@
+
+//....Falsy ;;-
+// 1- 0;
+//2- "";
+//3- undifaine;
+//4- null;
+//5- NaN ;
+//...Truely--
+//1- "0";
+//2- " ";
+//3- [];
+//4- {};
+
+const age = 44;
+
+if(age){
+     console.log("condition is true")
+}
+else{
+    console.log("condition is false");
+}
